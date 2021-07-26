070c4b6658f25f59a22fbf8d894bff8d61aafa78
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-26 22:38:51 +0900

    Create en.h

---
+++ lang/en.h
@@ -0,0 +1,5 @@
+char main_en[][] = {"    Terminal - main menu  ", "      AhnTri Terminal        \n", "Type help for help or o", "ther key for each apps.", ": command not found"}
+char ccalc_en[][] = {"\n    Start - AhnTri CCalc      ", "\n  [ Welcome to AhnTri Calc ], ", "For More, press [0] for help.", "Enter first number: ", "Enter second number: ", "Enter third number: ", "Your a: ", "Your b: ", "Addition:1, Sub:2, Mul:3, Div:4, Power 2:5, Power 3:6, sqrt: [10]"}
+char notes_en[][] = {"\nWelcome to AhnTri NoteTaker.\n", "Hint: press [Esc] or [Tab] to exit AhnTri NoteTaker\n"}
+char cal_en[][] = {"Calendar - Year 2021"}
+char timer[][] = {"AhnTri Stopwatch", "sec", "min", "h"}
