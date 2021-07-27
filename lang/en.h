246cfa0f6c2bed2c9bd962b1b8db2673f947ac2e
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-27 18:12:08 +0900

    Update en.h

--- lang/en.h
+++ lang/en.h
@@ -2,4 +2,4 @@ char main_en[][] = {"    Terminal - main menu  ", "      AhnTri Terminal        
 char ccalc_en[][] = {"\n    Start - AhnTri CCalc      ", "\n  [ Welcome to AhnTri Calc ], ", "For More, press [0] for help.", "Enter first number: ", "Enter second number: ", "Enter third number: ", "Your a: ", "Your b: ", "Addition:1, Sub:2, Mul:3, Div:4, Power 2:5, Power 3:6, sqrt: [10]"}
 char notes_en[][] = {"\nWelcome to AhnTri NoteTaker.\n", "Hint: press [Esc] or [Tab] to exit AhnTri NoteTaker\n"}
 char cal_en[][] = {"Calendar - Year 2021"}
-char timer[][] = {"AhnTri Stopwatch", "sec", "min", "h"}
+char timer_en[][] = {"AhnTri Stopwatch", "sec", "min", "h"}
