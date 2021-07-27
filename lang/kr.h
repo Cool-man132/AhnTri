f296680f814a89c9030f316dcee41ce51f361139
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-27 22:55:59 +0900

    Update kr.h

--- lang/kr.h
+++ lang/kr.h
@@ -1,3 +1,5 @@
+//Korean Romanized language pack
+//Copyright 2021 Jihwan Ahn
 char main_en[][] = {"    Teomineol - Giebon Menu  ", "      AhnTri Teomieol        \n", "doum-mal-eun ""help"", gita app silhayengen", "daren myungrhyungurhel iblyukhasio", ": myungrhyungu chjelsoo upsem"}
 char ccalc_en[][] = {"\n    Sijak - AhnTri CGesangi      ", "\n  [ AhnTri Gesangiea oshingussel hwanyunghamnidah ], ", "Ttohan, [0]uel ibryukhamyeon doum-mal naomnida", "Chu-uum sudjarel iblyukhasio: ", "Daum sudjarel iblyukhasio: ", "Sebunchae sudjarel iblyukhasio: ", "a-gapseul iblyukhasio: ", "b-gapseul iblyukhasio: ", "Duhah-gi:1, Ppei-gi:2, Gophah-gi:3, Nanuh-gi:4, Jjaegop:5, Sae-jaegop:6, Jaegop-geun(root): [10]"}
 char notes_en[][] = {"\nWelcome to AhnTri NoteTaker.\n", "Hint: press [Esc] or [Tab] to exit AhnTri NoteTaker\n"}
