c954a85d466fa436e86cce4159b4d1ea403cb268
Jihwan Ahn <39773400+AhnJihwan@users.noreply.github.com>
2021-07-27 22:53:26 +0900

    Create kr.h

---
+++ lang/kr.h
@@ -0,0 +1,5 @@
+char main_en[][] = {"    Teomineol - Giebon Menu  ", "      AhnTri Teomieol        \n", "doum-mal-eun ""help"", gita app silhayengen", "daren myungrhyungurhel iblyukhasio", ": myungrhyungu chjelsoo upsem"}
+char ccalc_en[][] = {"\n    Sijak - AhnTri CGesangi      ", "\n  [ AhnTri Gesangiea oshingussel hwanyunghamnidah ], ", "Ttohan, [0]uel ibryukhamyeon doum-mal naomnida", "Chu-uum sudjarel iblyukhasio: ", "Daum sudjarel iblyukhasio: ", "Sebunchae sudjarel iblyukhasio: ", "a-gapseul iblyukhasio: ", "b-gapseul iblyukhasio: ", "Duhah-gi:1, Ppei-gi:2, Gophah-gi:3, Nanuh-gi:4, Jjaegop:5, Sae-jaegop:6, Jaegop-geun(root): [10]"}
+char notes_en[][] = {"\nWelcome to AhnTri NoteTaker.\n", "Hint: press [Esc] or [Tab] to exit AhnTri NoteTaker\n"}
+char cal_en[][] = {"2021-nyun dalyuk"}
+char timer_en[][] = {"AhnTri Stapwatch", "cho", "boon", "sie"}
